bc7cc6b7ef29c0b81b3e24b69e0521c725874a84
Sarah Colby <scolby@austin.utexas.edu>
2018-03-09 10:29:23 -0600

    login required to access post editing

--- blog/views.py
+++ blog/views.py
@@ -13,6 +13,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -25,6 +26,7 @@ def post_new(request):
         form = PostForm()
         return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -38,6 +40,7 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
         return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
@@ -51,6 +54,7 @@ def publish(self):
     self.published_date = timezone.now()
     self.save()
 
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()

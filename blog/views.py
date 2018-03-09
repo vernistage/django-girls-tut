23575cda544156b8ace01a396096b7f77f3efe51
Sarah Colby <scolby@austin.utexas.edu>
2018-03-09 09:44:25 -0600

    save posts as drafts

--- blog/views.py
+++ blog/views.py
@@ -20,7 +20,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -34,7 +33,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
